6cdde29ecd294796d8c26baa2798f2256db3bc37
masheriprastio <162729373+masheriprastio@users.noreply.github.com>
2025-11-12 14:45:09 +0700

    build: include tinymce-init in app bundle

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Import global bootstrap utilities
+import './bootstrap';
+
+// Import TinyMCE initializer so it's bundled and available as window.initTiny
+import './tinymce-init';
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
