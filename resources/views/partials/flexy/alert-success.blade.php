89b9c859f3cb1ea18585516937e70a8691d0cef2
masheriprastio <162729373+masheriprastio@users.noreply.github.com>
2025-11-12 13:34:06 +0700

    feat(view): add flexy alert partials (alert-success, alert-errors)

---
+++ resources/views/partials/flexy/alert-success.blade.php
@@ -0,0 +1,6 @@
+@if(!empty($msg))
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    {!! $msg !!}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+@endif
